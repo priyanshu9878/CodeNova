a73e447f6ab095fdf528885a6c363236aef7ea6b
Priyanshu <priyanshu4096@gmail.com>
2026-08-01 21:20:14 +0530

    Use Vite environment variable for backend URL

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials:true,   // browser attach token
-    headers:{
-        'Content-Type': 'application/json'
-    }
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
