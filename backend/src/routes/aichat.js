0f9f660e58ff8a51975e35fcae2f1bb6296129de
Priyanshu <priyanshu4096@gmail.com>
2026-08-02 12:10:23 +0530

    Fix aiLimiter import path

--- backend/src/routes/aichat.js
+++ backend/src/routes/aichat.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { userMiddleware } from "../middleware/usermiddleware.js";
 import {solveDoubt} from "../controllers/solveDoubt.js";
-import { aiLimiter } from "../middleware/Ailimiter.js";
+import { aiLimiter } from "../middleware/aiLimiter.js";
 
 const chatRouter = express.Router();
 
